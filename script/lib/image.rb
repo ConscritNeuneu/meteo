7e9577a4ac7d1d85dfe6a57a8290d8efd8b2e792
Quentin Godfroy <quentin@godfroy.eu>
2014-07-13 18:39:52 +0200

    Move lib to sub-module Lib

--- script/lib/image.rb
+++ script/lib/image.rb
@@ -1,15 +1,17 @@
 module Meteo
-  def load_image(image_file)
-    File.open(image_file, "rb") do |file|
-      format = file.gets.chomp
-      width, height = file.gets.chomp.split.map { |i| i.to_i }
-      bits = file.read.unpack("C*")
+  module Lib
+    def load_image(image_file)
+      File.open(image_file, "rb") do |file|
+        format = file.gets.chomp
+        width, height = file.gets.chomp.split.map { |i| i.to_i }
+        bits = file.read.unpack("C*")
 
-      Bitmap.new(bits, width, height)
+        Bitmap.new(bits, width, height)
+      end
     end
-  end
 
-  def print_image(image_file)
-    load_image(image_file).print_bitmap
+    def print_image(image_file)
+      load_image(image_file).print_bitmap
+    end
   end
 end
