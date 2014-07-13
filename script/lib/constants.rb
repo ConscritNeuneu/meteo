47736d2b09c1e699fbf0c1ba45b7b0d6b7552017
Quentin Godfroy <quentin@godfroy.eu>
2014-07-13 20:35:43 +0200

    Add ESC_POS constants

---
+++ script/lib/constants.rb
@@ -0,0 +1,9 @@
+module Meteo
+  module Lib
+
+    ESC_POS_CENTER = "\ea\1"
+    ESC_POS_CP_1252 = "\et\x10"
+    ESC_POS_CUT = "\x1dVA\0"
+
+  end
+end
--- script/meteo.rb
+++ script/meteo.rb
@@ -1,4 +1,5 @@
 require_relative "lib/bitmap"
+require_relative "lib/constants"
 require_relative "lib/image"
 
 require_relative "radar/buienradar"
