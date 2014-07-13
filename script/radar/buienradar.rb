2e6f0bfd3eea41685c561d2c426aa9b393d55af0
Quentin Godfroy <quentin@godfroy.eu>
2014-07-13 21:49:03 +0200

    Use binding to ImageMagick instead of system()

--- script/radar/buienradar.rb
+++ script/radar/buienradar.rb
@@ -1,39 +1,30 @@
+require "RMagick"
 require "net/http"
-require "tempfile"
 
 module Meteo
   module Radar
     include Lib
+    include Magick
 
-    def get_radar_info(time)
+    def get_radar(time)
       uri = URI.parse("http://www2.buienradar.nl/zoomin/49.5.1.5.#{"%d%02d%02d.%02d%02d" % [time.year, time.month, time.day, time.hour, time.min]}.eu.gif")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
       response = http.request(request)
 
       if response.code == '200'
-        file = Tempfile.new(['rain', '.gif'], :encoding => 'ascii-8bit')
-        file.write(response.body)
-        file.close
-
-        file.path
+        response.body
       end
     end
 
     def construct_image(time)
-      # convert staticmap.png 49.5.1.5.20131028.0115.eu.gif -composite -dither Riemersma -monochrome ploum.pbm
-
-      radar_file = get_radar_info(time)
-      if radar_file
-        output_file = Tempfile.new(['rain', '.pbm'], :encoding => 'ascii-8bit')
-        ret = system("convert #{File.dirname(__FILE__)}/ile_de_france.png #{radar_file} -composite -dither Riemersma -monochrome #{output_file.path}")
-        if ret
-          output_file.rewind
-          output_pbm = output_file.read
-          output_file.close
-
-          print_image(output_pbm)
-        end
+      radar_data = get_radar(time)
+      if radar_data
+        radar = Image.from_blob(radar_data).first
+        idf = ImageList.new("#{File.dirname(__FILE__)}/ile_de_france.png").first
+        composed_image = idf.composite(radar, 0, 0, OverCompositeOp).quantize(2)
+        composed_image.format = "PBM"
+        print_image(composed_image.to_blob)
       end
     end
 
