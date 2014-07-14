c5926c2d69cea6d7867a6e20105f9506ae52d6b4
Quentin Godfroy <quentin@godfroy.eu>
2014-07-14 23:40:33 +0200

    Rename radar_des_pluies into releve_meteo
    It does more now

--- releve_meteo.rb
+++ releve_meteo.rb
@@ -3,9 +3,13 @@ require "tempfile"
 require "./script/meteo"
 
 include Meteo::Radar
+include Meteo::TempChart
 
 file = Tempfile.new(["meteo", ".img"], :encoding => "ascii-8bit")
-file.write(get_last_radar_image + ESC_POS_CUT)
+file.write(get_last_radar_image)
+file.write("-" * 48 + "\n")
+file.write(get_last_temp_chart)
+file.write(ESC_POS_CUT)
 file.close
 system("scp #{file.path} vega:/tmp")
 system("ssh vega 'cat /tmp/#{File.basename(file)} > /dev/usb/lp0'")
