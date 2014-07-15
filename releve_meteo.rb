03a053214b2281d8ee9fca3b7948e56268761812
Quentin Godfroy <quentin@godfroy.eu>
2014-07-16 00:46:42 +0200

    Get rid of last system() call

--- releve_meteo.rb
+++ releve_meteo.rb
@@ -6,13 +6,11 @@ include Meteo::Forecast
 include Meteo::Radar
 include Meteo::TempChart
 
-file = Tempfile.new(["meteo", ".img"], :encoding => "ascii-8bit")
-file.write(get_forecast)
-file.write("-" * 48 + "\n")
-file.write(get_last_radar_image)
-file.write("-" * 48 + "\n")
-file.write(get_last_temp_chart)
-file.write(ESC_POS_CUT)
-file.close
-system("scp #{file.path} vega:/tmp")
-system("ssh vega 'cat /tmp/#{File.basename(file)} > /dev/usb/lp0'")
+File.open("/dev/usb/lp0", "wb") do |printer|
+  printer.write(get_forecast)
+  printer.write("-" * 48 + "\n")
+  printer.write(get_last_radar_image)
+  printer.write("-" * 48 + "\n")
+  printer.write(get_last_temp_chart)
+  printer.write(ESC_POS_CUT)
+end
