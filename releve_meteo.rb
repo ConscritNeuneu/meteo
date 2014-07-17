0c6ee583b8171a7b140dec12559b979135c1ecf7
Quentin Godfroy <quentin@godfroy.eu>
2014-07-17 22:32:02 +0200

    Move text generation to a method

--- releve_meteo.rb
+++ releve_meteo.rb
@@ -6,11 +6,19 @@ include Meteo::Forecast
 include Meteo::Radar
 include Meteo::TempChart
 
+def releve_meteo
+  [
+    ESC_POS_INIT,
+    get_forecast,
+    "-" * 48 + "\n",
+    get_last_radar_image,
+    "-" * 48 + "\n",
+    get_last_temp_chart,
+    ESC_POS_CUT
+  ].join
+end
+
+
 File.open("/dev/usb/lp0", "wb") do |printer|
-  printer.write(get_forecast)
-  printer.write("-" * 48 + "\n")
-  printer.write(get_last_radar_image)
-  printer.write("-" * 48 + "\n")
-  printer.write(get_last_temp_chart)
-  printer.write(ESC_POS_CUT)
+  printer.write(releve_meteo)
 end
